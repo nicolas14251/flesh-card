886c9890170d79a611bb83f10bd6f0481163adc1
nicolas14251 <159157641+nicolas14251@users.noreply.github.com>
2024-10-08 12:32:32 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,22 @@
+criaCartao(
+    'Progamação',
+    'O que é JavaScript?',
+    'O JavaScript é uma linguagem de progamação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+criaCartao(
+    'Progamação',
+    'O que é uma função?',
+    'Uma função é um bloco de códigos que executa uma função'
+
+)
+criaCartao(
+    'Matemática',
+    'Qual é o resultado de 6 x 8?',
+    'O resultado é 48'
+)
